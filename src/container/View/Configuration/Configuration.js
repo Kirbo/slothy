5347cdbd46192e5216a6775381685302a4c8187f
Kimmo Saari <kimmo.saari@compile.fi>
2019-04-18 15:43:09 +0300

    Sorting rows on configurations view

--- src/container/View/Configuration/Configuration.js
+++ src/container/View/Configuration/Configuration.js
@@ -13,14 +13,37 @@ const { Header, Content } = Layout;
 const Configuration = () => (
   <Consumer>
     {({ configurations, viewType, selectedView, slackInstances, currentSsids }) => {
-      const dataSource = configurations.map(config => ({
-        key: config.id,
-        ssid: config.ssid,
-        bssid: config.bssid,
-        connected: !!currentSsids.find(ssid => config.bssid === ssid.bssid || config.ssid === ssid.ssid),
-        instanceId: config.instanceId,
-        config,
-      }));
+      const dataSource = configurations
+        .map(config => {
+          const instance = slackInstances.find(({ id }) => id === config.instanceId) || {
+            name: 'Unkown - deleted?'
+          };
+          return {
+            key: config.id,
+            slackInstanceName: instance.name,
+            ssid: config.ssid,
+            bssid: config.bssid,
+            connected: !!currentSsids.find(ssid => config.bssid === ssid.bssid || config.ssid === ssid.ssid),
+            instanceId: config.instanceId,
+            config,
+          };
+        })
+        .sort((a, b) => {
+          if (a.slackInstanceName < b.slackInstanceName) {
+            return -1;
+          } else if (a.slackInstanceName > b.slackInstanceName) {
+            return 1;
+          } else if (a.slackInstanceName === b.slackInstanceName && a.ssid < b.ssid) {
+            return -1;
+          } else if (a.slackInstanceName === b.slackInstanceName && a.ssid > b.ssid) {
+            return 1;
+          } else if (a.slackInstanceName === b.slackInstanceName && a.ssid === b.ssid && a.bssid < b.bssid) {
+            return -1;
+          } else if (a.slackInstanceName === b.slackInstanceName && a.ssid === b.ssid && a.bssid > b.bssid) {
+            return 1;
+          }
+          return 0;
+        });
 
       return (
         <Styled>
